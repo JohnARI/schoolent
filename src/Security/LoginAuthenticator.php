db896835e1e79861b3ce3196ac4455986a4a2ee1
Takabait <a.takabait@gmail.com>
2022-02-07 15:54:48 +0100

    init

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -50,7 +50,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('login'));
+        return new RedirectResponse($this->urlGenerator->generate('dashboard'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
