f5fae39460d6cc8f66b0914c08dda5069d618d21
Gaultier <90911279+Paul-Gaultier@users.noreply.github.com>
2022-02-11 11:21:44 +0100

    Rec 4

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -40,12 +40,10 @@ class RegisterController extends AbstractController
         $temporaryPassword= $passwordGenerator->passwordAleatoire(20);
         switch ($roles) {
             case "Formateur":
-                $this->addFlash('success', 'le mail a été envoyé avec succès!');
-                return $this->redirectToRoute('dashboard');
+                return $this->redirectToRoute('login');
                 break;
 
             case "Eleve":
-                $this->addFlash('success', 'le mail a été envoyé avec succès!');
                 return $this->redirectToRoute('dashboard');
                 break;
 
@@ -91,7 +89,6 @@ class RegisterController extends AbstractController
                     $this->entityManager->flush();
 
                     $this->mailjet->sendEmail($user, 'Bienvenue Chez SCHOOLENT! Voici votre mot de passe temporaire :'   .$temporaryPassword);
-                    $this->addFlash('success', 'le mail a été envoyé avec succès!');
                     $this->redirectToRoute('dashboard');
                 }
                 return $this->render('administration/admin/add_users.html.twig', [
