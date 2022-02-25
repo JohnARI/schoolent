d1ca4fb2e0b0c402ce6ebc6b5c0653aa0a1fac35
JohnARI <90911153+JohnARI@users.noreply.github.com>
2022-02-25 12:58:40 +0100

    ajout rémunération +page par defaut

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,25 +15,32 @@ class DefaultController extends AbstractController
      * @Route("/", name="default")
      */
     public function index(): Response
-    
+
     {
 
         if ($this->isGranted('ROLE_USER') == false) {
             return $this->redirectToRoute('login');
-        }
-         elseif($this->getUser()->getRole() == 'Administrateur') {
-            return $this->redirectToRoute('dashboard-admin');
-        }
-         elseif($this->getUser()->getRole() == 'Formateur') {
-            return $this->redirectToRoute('dashboard-teacher');
-        }
-         elseif($this->getUser()->getRole() == 'Eleve') {
-            return $this->redirectToRoute('dashboard-student');
-        }
+        } elseif ($user = $this->getUser()) {
+            $role = $user->getRoles();
 
-      
-    
+            switch ($role[0]) {
 
+                case 'ROLE_ADMIN':
+
+                    return $this->redirectToRoute('dashboard-admin');
+                    break;
+
+                case 'ROLE_TEACHER':
+
+                    return $this->redirectToRoute('dashboard-teacher');
+                    break;
+
+
+                case 'ROLE_USER':
+
+                    return $this->redirectToRoute('dashboard-student');
+                    break;
+            }
+        }
     }
 }
-
